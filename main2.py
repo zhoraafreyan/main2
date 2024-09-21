9e8b272f9bc5ccec283ac41caed89f21a9cca775
zhoraafreyan <zondalift@gmail.com>
2024-09-22 01:10:32 +0600

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,7 @@
+how_many_homework = (12)
+how_many_tames = (1.5)
+сourse_name = ('Pyton')
+time_for_one_task = (how_many_tames / how_many_homework)
+
+print("Курс:", сourse_name,",", "всего задач:", how_many_homework,",", "затрачено часов:", how_many_tames,",", "среднее время выполнения:", time_for_one_task,
+  "часов."    )
